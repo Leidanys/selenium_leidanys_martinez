101dbabdfc8a929a63592195a1ab3be466e8ca69
DESKTOP-IELEEJF\L&D <leidanys14@gmail.com>
2021-05-13 23:40:38 -0300

    Cambios: - Se actualiza método setup: Se agrega el inicio de sesión del navegador. Maximizar y refrescar página. - Se cambia el nombre de variable check por checkboxRememberElement. Método: rememberMeIsSelected. - Se cambia nombre de variable logo por  logoElement. Método: loginFailureTest - Se elimina método cleanCookies.

--- TestngSalesforce.java
+++ TestngSalesforce.java
@@ -15,7 +15,6 @@ public class TestngSalesforce {
     private static String ROOT_DIR = System.getProperty("user.dir") + File.separator;
     private static String DRIVERS_DIR = ROOT_DIR + "driver" + File.separator;
     private static String CHROME_PATH = DRIVERS_DIR + "chromedriver.exe";
-
     private static  String SALEFORCE_URL= "https://login.salesforce.com/?locale=eu";
 
     @BeforeMethod
@@ -23,13 +22,13 @@ public class TestngSalesforce {
 
         System.setProperty("webdriver.chrome.driver",CHROME_PATH);
         driverChrome=new ChromeDriver();
+        driverChrome.get(SALEFORCE_URL);
+        driverChrome.manage().window().maximize();
+        driverChrome.navigate().refresh();
     }
 
     @Test(priority = 1,groups = {"successTests"},enabled = true)
     public void validateSalesforceLogoTest(){
-         driverChrome.get(SALEFORCE_URL);
-         driverChrome.manage().window().maximize();
-         driverChrome.navigate().refresh();
 
         String tagname=driverChrome.findElement(By.id("logo")).getTagName();
         WebElement icono =driverChrome.findElement(By.id("logo"));
@@ -41,27 +40,20 @@ public class TestngSalesforce {
 
     @Test(priority = 4, groups = {"successTests"})
     public void rememberMeIsSelected()throws Exception{
-       driverChrome.get("https://login.salesforce.com/?locale=eu");
-       driverChrome.manage().window().maximize();
-       driverChrome.navigate().refresh();
-
        Thread.sleep(2000);
-       WebElement check= driverChrome.findElement(By.id("rememberUn"));
-       if (!check.isSelected()){
+       WebElement checkboxRememberElement= driverChrome.findElement(By.id("rememberUn"));
+       if (!checkboxRememberElement.isSelected()){
 
-           check.click();
+           checkboxRememberElement.click();
        }
-        Assert.assertTrue(check.isSelected(), "Se esperaba que el elemento: "+check.getText()+" estuviera seleccionado");
+        Assert.assertTrue(checkboxRememberElement.isSelected(), "Se esperaba que el elemento: "+checkboxRememberElement.getText()+" estuviera seleccionado");
         Thread.sleep(2000);
     }
 
     @Test(priority = 2,enabled = false ,groups = {"fail test"})
     public void FooterIsValid() throws Exception {
-        driverChrome.get(SALEFORCE_URL);
-        driverChrome.manage().window().maximize();
-        driverChrome.navigate().refresh();
-        Thread.sleep(2000);
 
+        Thread.sleep(2000);
         String textoEsperado="All rights reserved";
         WebElement footer= driverChrome.findElement(By.id("footer"));
 
@@ -72,14 +64,12 @@ public class TestngSalesforce {
     //Ejercicio GIT
     @Test(priority = 3)
     public void loginFailureTest(@Optional("test@test.com") String emailParameter, @Optional("123466") String passwordParameter) throws Exception{
-        driverChrome.get(SALEFORCE_URL);
-        driverChrome.manage().window().maximize();
-        driverChrome.navigate().refresh();
+
         Assert.assertEquals(driverChrome.getCurrentUrl(),SALEFORCE_URL, "No coincide la url");
 
         Thread.sleep(2000);
-        WebElement logo= driverChrome.findElement(By.id("logo"));
-        Assert.assertTrue(logo.isDisplayed(),"Se esperaba encontrar el logo en el sitio");
+        WebElement logoElement= driverChrome.findElement(By.id("logo"));
+        Assert.assertTrue(logoElement.isDisplayed(),"Se esperaba encontrar el logoElement en el sitio");
 
         WebElement email= driverChrome.findElement(By.id("username"));
         email.sendKeys(emailParameter);
@@ -93,35 +83,22 @@ public class TestngSalesforce {
         WebElement error= driverChrome.findElement(By.id("error"));
         System.out.println("Mensaje: "+ error.getText());
         Assert.assertTrue(error.isDisplayed());
-
-
     }
 
-
-    @AfterMethod
-    private void  cleanCookies() throws Exception  {
-
-
-        try {
-            System.out.println("---->Limpiando cookies <-----------");
-            driverChrome.manage().deleteAllCookies();
-
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
 
     @AfterMethod
     private void closeDriver() throws Exception  {
 
-
         try {
+             Thread.sleep(2000);
             driverChrome.close();
 
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
+
+
 
 
 }
