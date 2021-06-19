b8a2b4486d18f6822a174fdc7d556578b5f5c6a7
DESKTOP-IELEEJF\L&D <leidanys14@gmail.com>
2021-06-19 20:59:49 -0300

    Archivos examen

---
+++ DataProviderTest.java
@@ -0,0 +1,19 @@
+package ExamenFinal;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderTest {
+
+    public static FakerTest fakerTest=new FakerTest();
+
+    @DataProvider(name = "email")
+    public Object[][] dataEmail(){
+        return new Object[][]{
+                {fakerTest.getEmail()},
+                {fakerTest.getEmail()},
+                {fakerTest.getEmail()},
+    };
+    }
+
+
+}
